1a3408fbb41db4e60c0bef6a8b67dea668e002ad
Francis <francisgacutno39@gmail.com>
2026-04-06 22:38:53 +0800

    removed the base property

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ultimate-profile/',
+  // Remove or comment out the line below
+  // base: '/ultimate-profile/',
 })
